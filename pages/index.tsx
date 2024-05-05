65692f4811dac8b24686a9c4df98e9386877cbce
Ahmed Ebrahim Elgamal <ahmedelgamal2000@gmail.com>
2024-05-06 00:38:29 +0300

    Music Control Bar Design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <>Hello</>;
-}
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+
+const HomePage = () => {
+  return (
+    <div style={{ flexGrow: 1, height: '100vh', position: 'relative' }}>
+      <Grid container spacing={3} style={{ padding: '24px', textAlign: 'center' }}>
+        <Grid item xs={12}>
+          <Typography variant="h3" style={{ color: 'blue', marginBottom: '16px' }}>
+            Welcome to SpaceTune
+          </Typography>
+        </Grid>
+      </Grid>
+      
+    </div>
+  );
+};
+
+export default HomePage;
