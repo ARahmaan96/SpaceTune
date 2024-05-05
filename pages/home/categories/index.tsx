74c4d3cfe6cd4c7eda63fd955b705393edc0402a
Ahmed Elghabour <ahmedelghabour6@gmail.com>
2024-05-06 02:24:34 +0300

    some fixes

--- pages/home/categories/index.tsx
+++ pages/home/categories/index.tsx
@@ -1,46 +1,8 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import { Container, Grid, Typography } from "@mui/material";
-// import CategoryCard from "@/components/musicCards/CategoryCard";
-
-// const CategoriesPage = () => {
-//   const [categories, setCategories] = useState([]);
-
-//   useEffect(() => {
-//     const fetchCategories = async () => {
-//       try {
-//         const response = await axios.get("http://localhost:3001/categories");
-//         setCategories(response.data);
-//       } catch (error) {
-//         console.error("Error fetching categories:", error);
-//       }
-//     };
-
-//     fetchCategories();
-//   }, []);
-
-//   return (
-//     <Container>
-//       <Typography variant="h2" align="center" gutterBottom>
-//         Categories
-//       </Typography>
-//       <Grid container spacing={3}>
-//         {categories.map((category: any) => (
-//           <Grid item xs={12} sm={6} md={4} key={category.id}>
-//             <CategoryCard title={category.name} imageUrl={category.image} />
-//           </Grid>
-//         ))}
-//       </Grid>
-//     </Container>
-//   );
-// };
-
-// export default CategoriesPage;
-
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Container, Grid, Typography } from "@mui/material";
 import CategoryCard from "@/components/musicCards/CategoryCard";
+import { useRouter } from "next/router";
 
 const CategoriesPage = () => {
   const [categories, setCategories] = useState([]);
@@ -48,7 +10,8 @@ const CategoriesPage = () => {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/categories");
+        const response = await axios.get("/api/home/categories");
+        console.log(response.data);
         setCategories(response.data);
       } catch (error) {
         console.error("Error fetching categories:", error);
