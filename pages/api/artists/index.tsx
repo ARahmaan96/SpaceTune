b1fd243e4be8b0edd7e6116b7b3b8294c8e9d2af
Ahmed Elghabour <ahmedelghabour6@gmail.com>
2024-05-08 21:08:16 +0300

    add artist works now

--- pages/api/artists/index.tsx
+++ pages/api/artists/index.tsx
@@ -1,10 +1,18 @@
 import artists from "@/db/artists.json";
+import fs from "fs";
+import path from "path";
+
 export default async function handler(req: any, res: any) {
   try {
     if (req.method === "GET") {
       res.status(200).json(artists);
     } else if (req.method === "POST") {
-      // Handle POST request if needed
+      const newArtist = req.body;
+      console.log(newArtist);
+
+      artists.push(newArtist);
+      saveTracksToJSON();
+      res.status(200).json();
     } else {
       res.status(405).json({ message: "Method not allowed!" });
     }
@@ -12,4 +20,9 @@ export default async function handler(req: any, res: any) {
     console.error(err);
     res.status(500).json({ message: "Something went wrong" });
   }
+}
+
+function saveTracksToJSON() {
+  const filePath = path.resolve(__dirname, "../../../../db/artists.json");
+  fs.writeFileSync(filePath, JSON.stringify(artists, null, 2));
 }
---
+++ pages/home/artists/add.tsx
@@ -0,0 +1,179 @@
+import axios from "axios";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { v4 as uuid } from "uuid";
+import { useRouter } from "next/router";
+
+function AddArtist() {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    artist_name: "",
+    nationality: "",
+    language: "",
+    image_url: "",
+    age: "",
+    no_of_albums: "",
+    no_of_songs: "",
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const newArtist = {
+        ...formData,
+        id: uuid(),
+      };
+
+      await axios.post("/api/artists", newArtist);
+
+      setFormData({
+        artist_name: "",
+        nationality: "",
+        language: "",
+        image_url: "",
+        age: "",
+        no_of_albums: "",
+        no_of_songs: "",
+      });
+      alert("Artist added successfully!");
+      router.push("/");
+    } catch (error) {
+      console.error("Error adding Artist:", error);
+      alert("Failed to add Artist. Please try again.");
+    }
+  };
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        marginBottom: "20px",
+        top: "1px",
+        left: "20px",
+        right: "0vw",
+        bottom: "55px",
+        minWidth: "80vw",
+        overflowY: "auto",
+      }}
+    >
+      <Form
+        className="p-5 m-auto"
+        onSubmit={handleSubmit}
+        style={{
+          paddingBottom: "100px",
+          display: "flex",
+          flexDirection: "column",
+          position: "absolute",
+          top: "1px",
+          left: "15vw",
+          right: "5vw",
+          bottom: "100px",
+          minWidth: "80vw",
+          maxWidth: "80vw",
+        }}
+      >
+        <Form.Group className="mb-3" controlId="formBasicArtistName">
+          <Form.Label>Artist Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter artist name"
+            name="artist_name"
+            value={formData.artist_name}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicNationality">
+          <Form.Label>Nationality</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter nationality"
+            name="nationality"
+            value={formData.nationality}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicLanguage">
+          <Form.Label>Language</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter language"
+            name="language"
+            value={formData.language}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicImageUrl">
+          <Form.Label>Artist Image URL</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter artist image URL"
+            name="image_url"
+            value={formData.image_url}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicAge">
+          <Form.Label>Age</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Enter age"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicNoOfAlbums">
+          <Form.Label>Number of Albums</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Enter number of albums"
+            name="no_of_albums"
+            value={formData.no_of_albums}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicNoOfSongs">
+          <Form.Label>Number of Songs</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Enter number of songs"
+            name="no_of_songs"
+            value={formData.no_of_songs}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Button
+          variant="contained"
+          type="submit"
+          style={{ width: "100%", marginTop: 2, backgroundColor: "salmon" }}
+        >
+          ADD
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default AddArtist;
